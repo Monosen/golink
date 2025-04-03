65059723d0f9413652d31171b6e70613f2f66e64
monosen <edinson.bolanos00@usc.edu.co>
2025-04-02 20:39:11 -0500

    style(web): add styles to navbar component

--- apps/web/src/app/components/navbar/navbar.component.ts
+++ apps/web/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core'
 import { RouterLink } from '@angular/router'
+import { LoginIconComponent } from '../../assets/icons/login-icon/login-icon.component'
+import { RegisterIconComponent } from '../../assets/icons/register-icon/register-icon.component'
 
 @Component({
     selector: 'app-navbar',
-    imports: [RouterLink],
+    imports: [RouterLink, LoginIconComponent, RegisterIconComponent],
     templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {}
