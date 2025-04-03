b9291bb1f3013d6b92c2707e840c2670db28fe63
monosen <edinson.bolanos00@usc.edu.co>
2025-04-02 20:37:56 -0500

    feat(web): add styles for Home and Dashboard pages

--- apps/web/src/app/pages/dash/dash.component.ts
+++ apps/web/src/app/pages/dash/dash.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core'
-import { NavbarComponent } from '../../components/navbar/navbar.component'
 
 @Component({
     selector: 'app-dash',
-    imports: [NavbarComponent],
+    imports: [],
     templateUrl: './dash.component.html'
 })
 export class DashComponent {}
--- apps/web/src/app/pages/home/home.component.ts
+++ apps/web/src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core'
-import { NavbarComponent } from '../../components/navbar/navbar.component'
+import { ArrowRightIconComponent } from '../../assets/icons/arrow-right-icon/arrow-right-icon.component'
+import { GithubIconComponent } from '../../assets/icons/github-icon/github-icon.component'
+import { UrlLinkIconComponent } from '../../assets/icons/url-link-icon/url-link-icon.component'
 
 @Component({
     selector: 'app-home',
-    imports: [NavbarComponent],
+    imports: [GithubIconComponent, UrlLinkIconComponent],
     templateUrl: './home.component.html'
 })
 export class HomeComponent {}
