ae2c89b7e2f931ca91be6192ffd3d6fa6f0a4e26
monosen <edinson.bolanos00@usc.edu.co>
2025-04-02 20:42:01 -0500

    feat(web): add main layout structure

---
+++ apps/web/src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+import { NavbarComponent } from '../../components/navbar/navbar.component'
+
+@Component({
+    selector: 'app-main-layout',
+    imports: [NavbarComponent],
+    templateUrl: './main-layout.component.html'
+})
+export class MainLayoutComponent {}
