4b918feaabf5105a906d672631973871c490c06c
monosen <edinson.bolanos00@usc.edu.co>
2025-04-06 18:35:27 -0500

    feat(web): add new set of icons

---
+++ apps/web/src/app/assets/icons/copy-icon/copy-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+    selector: 'app-copy-icon',
+    imports: [],
+    templateUrl: './copy-icon.component.html'
+})
+export class CopyIconComponent {
+    @Input() color: string = 'currentColor'
+    @Input() svgClass: string = 'size-4'
+}
