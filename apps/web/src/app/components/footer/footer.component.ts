0b4a697c08ad09a25e1a0452243cba7a2e151f1b
monosen <edinson.bolanos00@usc.edu.co>
2025-04-06 03:21:52 -0500

    feat(web): add footer component

---
+++ apps/web/src/app/components/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { LinkedinIconComponent } from '../../assets/icons/linkedin-icon/linkedin-icon.component'
+import { HeartIconComponent } from '../../assets/icons/heart-icon/heart-icon.component'
+
+@Component({
+    selector: 'app-footer',
+    imports: [LinkedinIconComponent, HeartIconComponent],
+    templateUrl: './footer.component.html'
+})
+export class FooterComponent {}
