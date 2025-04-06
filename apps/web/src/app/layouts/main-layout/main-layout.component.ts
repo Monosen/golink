2a17740f29edab2b0aa01a780f83484b002199e9
monosen <edinson.bolanos00@usc.edu.co>
2025-04-06 03:41:19 -0500

    style(web): improve styling of main layout

--- apps/web/src/app/layouts/main-layout/main-layout.component.ts
+++ apps/web/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
 import { NavbarComponent } from '../../components/navbar/navbar.component'
+import { FooterComponent } from '../../components/footer/footer.component'
 
 @Component({
     selector: 'app-main-layout',
-    imports: [NavbarComponent],
+    imports: [NavbarComponent, FooterComponent],
     templateUrl: './main-layout.component.html'
 })
 export class MainLayoutComponent {}
