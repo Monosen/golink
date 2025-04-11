a0f89f79f8986151bab15552a9e9d9f7135fed03
monosen <edinson.bolanos00@usc.edu.co>
2025-04-10 21:28:25 -0500

    feat(web): enhance input component with reactive forms support and value accessor

--- apps/web/src/app/components/ui/input/input.component.ts
+++ apps/web/src/app/components/ui/input/input.component.ts
@@ -1,11 +1,40 @@
-import { Component, Input } from '@angular/core'
+import {
+    Component,
+    ElementRef,
+    Input,
+    Renderer2,
+    forwardRef
+} from '@angular/core'
+import {
+    ControlValueAccessor,
+    DefaultValueAccessor,
+    NG_VALUE_ACCESSOR,
+    ReactiveFormsModule
+} from '@angular/forms'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
 @Component({
     selector: 'app-input',
-    imports: [],
-    templateUrl: './input.component.html'
+    imports: [ReactiveFormsModule, CommonModule, FormsModule],
+    templateUrl: './input.component.html',
+    standalone: true,
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => InputComponent),
+            multi: true
+        }
+    ]
 })
-export class InputComponent {
+export class InputComponent
+    extends DefaultValueAccessor
+    implements ControlValueAccessor
+{
     @Input() placeholder: string = 'text'
     @Input() type: string = 'text'
+
+    constructor(renderer: Renderer2, elementRef: ElementRef) {
+        super(renderer, elementRef, false)
+    }
 }
