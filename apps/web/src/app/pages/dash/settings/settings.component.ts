fc64d5632bf87fed0905d5af88e784d794827731
monosen <edinson.bolanos00@usc.edu.co>
2025-04-10 21:18:41 -0500

    feat(web): create settings component with personal information and account management

---
+++ apps/web/src/app/pages/dash/settings/settings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { InputComponent } from '../../../components/ui/input/input.component'
+import { ButtonComponent } from '../../../components/ui/button/button.component'
+
+@Component({
+    selector: 'app-settings',
+    imports: [InputComponent, ButtonComponent],
+    templateUrl: './settings.component.html'
+})
+export class SettingsComponent {}
