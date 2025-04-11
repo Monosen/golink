2f6e1c7a24fbb1b90ab9dc502cddc24185f2f0a0
monosen <edinson.bolanos00@usc.edu.co>
2025-04-11 08:46:48 -0500

    style(web): enhance link management bar with search icon and improve button styles; update sub-navbar and settings page for better UI consistency

--- apps/web/src/app/components/link-management-bar/link-management-bar.component.ts
+++ apps/web/src/app/components/link-management-bar/link-management-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
 import { CreateNewLinkComponent } from '../modals/create-new-link/create-new-link.component'
+import { SearchIconComponent } from '../../assets/icons/search-icon/search-icon.component'
 
 @Component({
     selector: 'app-link-management-bar',
-    imports: [CreateNewLinkComponent],
+    imports: [CreateNewLinkComponent, SearchIconComponent],
     templateUrl: './link-management-bar.component.html'
 })
 export class LinkManagementBarComponent {
--- apps/web/src/app/components/sub-navbar/sub-navbar.component.ts
+++ apps/web/src/app/components/sub-navbar/sub-navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core'
-import { RouterLink } from '@angular/router'
+import { RouterLink, RouterModule } from '@angular/router'
 import { SettingIconComponent } from '../../assets/icons/setting-icon/setting-icon.component'
 import { LinkIconComponent } from '../../assets/icons/link-icon/link-icon.component'
 
 @Component({
     selector: 'app-sub-navbar',
-    imports: [RouterLink, SettingIconComponent, LinkIconComponent],
+    imports: [
+        RouterLink,
+        SettingIconComponent,
+        LinkIconComponent,
+        RouterModule
+    ],
     templateUrl: './sub-navbar.component.html'
 })
 export class SubNavbarComponent {}
