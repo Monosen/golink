21e892283db2270ddfe7823e35397a005f4b8432
monosen <edinson.bolanos00@usc.edu.co>
2025-04-10 21:20:28 -0500

    feat(web): refactor navbar component to enhance user authentication display and include additional icons

--- apps/web/src/app/components/navbar/navbar.component.ts
+++ apps/web/src/app/components/navbar/navbar.component.ts
@@ -3,6 +3,11 @@ import { RouterLink } from '@angular/router'
 import { LoginIconComponent } from '../../assets/icons/login-icon/login-icon.component'
 import { RegisterIconComponent } from '../../assets/icons/register-icon/register-icon.component'
 import { LinkComponent } from '../ui/link/link.component'
+import { AuthService } from '../../pages/auth/auth.service'
+import { UserIconComponent } from '../../assets/icons/user-icon/user-icon.component'
+import { GithubIconComponent } from '../../assets/icons/github-icon/github-icon.component'
+import { MoonIconComponent } from '../../assets/icons/moon-icon/moon-icon.component'
+import { UserDropdownComponent } from '../user-dropdown/user-dropdown.component'
 
 @Component({
     selector: 'app-navbar',
@@ -10,8 +15,16 @@ import { LinkComponent } from '../ui/link/link.component'
         RouterLink,
         LoginIconComponent,
         RegisterIconComponent,
-        LinkComponent
+        LinkComponent,
+        UserIconComponent,
+        GithubIconComponent,
+        MoonIconComponent,
+        UserDropdownComponent
     ],
     templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    constructor(public readonly authService: AuthService) {
+        // Initialize any necessary properties or services here
+    }
+}
