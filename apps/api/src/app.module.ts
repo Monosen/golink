7ad89f4414409d3724473dec1f8c496c22d55008
monosen <edinson.bolanos00@usc.edu.co>
2025-04-11 22:42:02 -0500

    feat(api): add ConfigModule to initialize configuration settings

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { PrismaModule } from './prisma/prisma.module'
 import { UserModule } from './user/user.module'
 import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-    imports: [PrismaModule, UserModule, AuthModule],
+    imports: [ConfigModule.forRoot(), PrismaModule, UserModule, AuthModule],
     controllers: [],
     providers: []
 })
