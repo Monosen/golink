bc188b678e1d32a39f2e6c59caadf435a70fdf78
monosen <edinson.bolanos00@usc.edu.co>
2025-04-27 09:20:45 -0500

    feat(web): enhance ShortUrlCard component with clipboard functionality and improve date formatting

--- apps/web/src/app/components/short-url-card/short-url-card.component.ts
+++ apps/web/src/app/components/short-url-card/short-url-card.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, Inject } from '@angular/core'
+import { DOCUMENT } from '@angular/common'
 import { CopyIconComponent } from '../../assets/icons/copy-icon/copy-icon.component'
 import { SettingIconComponent } from '../../assets/icons/setting-icon/setting-icon.component'
 import { TrashIconComponent } from '../../assets/icons/trash-icon/trash-icon.component'
 import { ShortLinkService } from '../../services/short-link.service'
+import { CheckIconComponent } from '../../assets/icons/check-icon/check-icon.component'
 
 @Component({
     selector: 'app-short-url-card',
-    imports: [CopyIconComponent, SettingIconComponent, TrashIconComponent],
+    imports: [
+        CopyIconComponent,
+        SettingIconComponent,
+        TrashIconComponent,
+        CheckIconComponent
+    ],
     templateUrl: './short-url-card.component.html'
 })
 export class ShortUrlCardComponent {
@@ -16,7 +23,15 @@ export class ShortUrlCardComponent {
     @Input() fullUrl: string = ''
     @Input() date: string = ''
 
-    constructor(private readonly shortLinkService: ShortLinkService) {}
+    isCopied: boolean = false // Nueva propiedad para rastrear el estado del ícono
+    baseUrl: string = '' // Propiedad para almacenar la URL base
+
+    constructor(
+        private readonly shortLinkService: ShortLinkService,
+        @Inject(DOCUMENT) private document: Document // Inyecta el objeto DOCUMENT
+    ) {
+        this.baseUrl = this.document.location.origin // Obtén la URL base
+    }
 
     removeShortUrl() {
         this.shortLinkService.deleteShortUrl(this.id).subscribe({
@@ -28,5 +43,36 @@ export class ShortUrlCardComponent {
                 console.error('Error removing short URL:', error)
             }
         })
+    }
+
+    getFormattedDate(): string {
+        const date = new Date(this.date)
+        return date.toLocaleDateString('en-US', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+        })
+    }
+
+    copyToClipboard() {
+        navigator.clipboard
+            .writeText(this.baseUrl + '/' + this.shortCode) // Copia la URL completa
+            .then(() => {
+                this.isCopied = true // Cambia al ícono de check
+                setTimeout(() => {
+                    this.isCopied = false // Vuelve al ícono de copy después de 2 segundos
+                }, 2000)
+            })
+            .catch((error) => {
+                console.error('Error copying to clipboard:', error)
+            })
+    }
+
+    getFullUrl() {
+        if (this.fullUrl.length > 35) {
+            return this.fullUrl.slice(0, 35) + '...'
+        }
+
+        return this.fullUrl
     }
 }
