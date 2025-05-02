cbfabb5fd9ad35cd2812b74456cb29f3190ec1ff
monosen <edinson.bolanos00@usc.edu.co>
2025-05-02 17:12:11 -0500

    refactor(web): replace custom clock icon component with iconify span for improved performance

--- apps/web/src/app/components/ui/time-picker/time-picker.component.ts
+++ apps/web/src/app/components/ui/time-picker/time-picker.component.ts
@@ -10,13 +10,12 @@ import {
   NG_VALUE_ACCESSOR,
   FormsModule,
 } from '@angular/forms'
-import { ClockIconComponent } from '../../../assets/icons/clock-icon/clock-icon.component'
 
 @Component({
   selector: 'app-time-picker',
   templateUrl: './time-picker.component.html',
   standalone: true,
-  imports: [FormsModule, ClockIconComponent],
+  imports: [FormsModule],
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
